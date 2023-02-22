5befe712ddf1c46de3f514518a77a11b02e46966
Serhat Ozturk <so2turk@yahoo.com>
2023-02-22 13:25:12 +0100

    Get geoJSON data from OSM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import axios from 'axios'
+import osmtogeojson from 'osmtogeojson'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-	return <></>
+	const [geoData, setGeoData] = useState({ GeoJSONData: [], error: '' })
+	const url = 'https://www.openstreetmap.org/api/0.6/map'
+	const [bbox, setBbox] = useState({
+		min_lon: 13.4,
+		min_lat: 52.5,
+		max_lon: 13.41,
+		max_lat: 52.51,
+	})
+
+	useEffect(() => {
+		const getGeoJSONData = async (bbox) => {
+			try {
+				const data = await axios.get(
+					`${url}?bbox=${bbox.min_lon},${bbox.min_lat},${bbox.max_lon},${bbox.max_lat}`
+				)
+				const convertedData = osmtogeojson(data.data)
+				setGeoData({ GeoJSONData: convertedData.features, error: '' })
+			} catch (err) {
+				setGeoData({ error: err.response.data })
+				console.log(err)
+			}
+		}
+
+		getGeoJSONData(bbox)
+	}, [])
+
+	return (
+		<>
+			<pre>{JSON.stringify(geoData, undefined, 2)}</pre>
+		</>
+	)
 }
 
 export default App
